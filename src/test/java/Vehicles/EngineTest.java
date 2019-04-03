fa9e9187296a8ac5709c3eb180bb902bada8586f
Abigaila Ekierte <arekiert@gmail.com>
2019-04-03 15:41:28 +0100

    engine testing completed

--- src/test/java/Vehicles/EngineTest.java
+++ src/test/java/Vehicles/EngineTest.java
@@ -1,4 +1,23 @@
 package Vehicles;
 
+import ArnoldCodeClan.Vehicles.Engine;
+import ArnoldCodeClan.Vehicles.EngineType;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class EngineTest {
+
+    Engine engine;
+
+    @Before
+    public void before() {
+        engine = new Engine(EngineType.DIESEL);
+    }
+
+    @Test
+    public void hasEngineType() {
+        assertEquals("Diesel", engine.getEngineType());
+    }
 }
